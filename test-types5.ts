1fab05d0bcbffe14b7d61721d4b311bdecf2ba50
resaleigolenaz-code <resaleigolenaz@gmail.com>
2026-08-21 14:33:26 -0400

    Delete test-types5.ts

--- test-types5.ts
+++
@@ -1,7 +0,0 @@
-import { useChat } from '@ai-sdk/react'
-
-function Test() {
-    const chat = useChat()
-    type SendMessageType = typeof chat.sendMessage;
-    const x: SendMessageType = null as any;
-}
