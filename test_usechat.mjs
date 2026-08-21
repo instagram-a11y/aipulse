0db6099354a3e177fc5ea070d0927917ac379837
resaleigolenaz-code <resaleigolenaz@gmail.com>
2026-08-21 13:51:18 -0400

    Create test_usechat.mjs

---
+++ test_usechat.mjs
@@ -0,0 +1,5 @@
+import React from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useChat } from '@ai-sdk/react';
+
+// Wait, testing react hooks in node requires a lot of setup. I'll just check the types.
