de416e58810a36f7cb69171d15a2d430e3326a96
resaleigolenaz-code <resaleigolenaz@gmail.com>
2026-08-21 14:33:28 -0400

    Delete test-types6.ts

--- test-types6.ts
+++
@@ -1,7 +0,0 @@
-import { useChat } from '@ai-sdk/react'
-
-function Test() {
-    const chat = useChat()
-    chat.append({ role: 'user', content: 'hi' }) // This errors
-    chat.sendMessage([{ role: 'user', content: 'hi' }]) // This errors
-}
